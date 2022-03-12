53a3b07791d487f105b9d6953f15e6c081b16b26
Szabo,Mate <Mate_Szabo1@epam.onmicrosoft.com>
2022-03-12 19:50:05 +0100

    revert App Keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ['ktdI8A51P5opyod437rtWQ==', 'lnIESS16DabjP8JWZL8ugw==', 'c4sWln46n82hONAS0HzcAQ==', 'm5NySc0C9G0N7WWRgTM/Ng==']),
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
